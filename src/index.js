e9a63efff8f1c12e2ba2cc456bb233ec2afeeab0
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-24 12:36:48 -0300

    Feat: Added structure styled

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './views/App'
+import './index.css'
 
 ReactDOM.render(
-    <h1>APP</h1>,
+    <App/>,
     document.getElementById('root')
 )
---
+++ src/views/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
+
+import './App.css'
+const App = props => (
+    <div className='App'>
+        <Menu/>
+        <Content></Content>
+
+    </div>
+)
+
+export default App
